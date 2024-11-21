f5a7d9fc44b6991742b2b040649a0fa5bdd9661c
itamarHenrique <itamarhjp1@gmail.com>
2024-11-20 23:57:33 -0300

    adição função editar aluno

--- exemplo1Acampamento/src/main/java/br/com/acampamento/exemplo1Acampamento/controller/AlunoController.java
+++ exemplo1Acampamento/src/main/java/br/com/acampamento/exemplo1Acampamento/controller/AlunoController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -44,6 +45,19 @@ public class AlunoController {
         }
 
         return null;
+    }
+
+    @PutMapping("/{matricula}")
+    public void editarAluno(@PathVariable int matricula, @RequestBody Aluno novosDadosAluno){
+
+        for(Aluno aluno : listaAlunos){
+            if(aluno.getMatricula() == matricula){
+                aluno.setNome(novosDadosAluno.getNome());
+                aluno.setSobrenome(novosDadosAluno.getSobrenome());
+                aluno.setCpf(novosDadosAluno.getCpf());
+                aluno.setMatricula(novosDadosAluno.getMatricula());
+            }
+        }
     }
 
     @DeleteMapping("/{matricula}")
