9ea0889bd55754ff0595faaa78bf1ce16df91b63
itamarHenrique <itamarhjp1@gmail.com>
2024-11-25 23:15:08 -0300

    Remover aluno e verificar se a lista está vazia

--- Java puro/alunos/src/application/Main.java
+++ Java puro/alunos/src/application/Main.java
@@ -1,23 +1,48 @@
 package application;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class Main {
 
+    private static List<String> alunos = new ArrayList<>();
+
     public static void main(String Args[]){
-        // System.out.println("Olá mundo");
 
-        List<String> alunos = new ArrayList<>();
+        adicionaAluno("Emanuel");
 
-        alunos.add("Pablo");
-        alunos.add("Jackson");
-        alunos.add("Henrique");
-        alunos.add("Larissa");
+        imprimirLista();
 
+        removeAluno("Emanuel");
+
+        imprimirLista();
+        
+
+
+    }
+
+    public static void adicionaAluno(String nome){
+        alunos.add(nome);
+    }
+
+    public static void imprimirLista(){
         for (String aluno : alunos) {
             System.out.println("O nome do aluno é: " + aluno);
+        }
+        
+        if(alunos.isEmpty()){
+            System.out.println("A lista está vazia");
+        }
+    }
 
+    public static void removeAluno(String nome){
+        Iterator<String> iterator = alunos.iterator();
+        while(iterator.hasNext()){
+            String aluno = iterator.next();
+            if(aluno.equals(nome)){
+                iterator.remove();
+            }
         }
     }
 
